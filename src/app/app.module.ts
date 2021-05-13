fd0e0c3e43db4d49b1e6600d7c78c0b2969869a9
filipef9 <filipef9@hotmail.com>
2021-05-13 15:11:26 +0000

    Limpeza de duplicação de imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,7 +26,6 @@ import { FilmesModule } from './filmes/filmes.module';
     LayoutModule,
     MaterialModule,
     AppRoutingModule,
-    FilmesModule
   ],
   providers: [{ provide: MAT_DATE_LOCALE, useValue: 'pt' }],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'https://3000-pink-marten-zb9jqwkw.ws-us04.gitpod.io'
 };
 
 /*
