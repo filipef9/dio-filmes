b632f99cd656d023a62da3d110dbfd32f005ce71
filipef9 <filipef9@hotmail.com>
2021-05-27 15:36:15 +0000

    Técnicas avançadas em Angular 8 - Buscando e listando filmes
    Listagem de resultados e melhoria de performance

--- src/app/core/filmes.service.ts
+++ src/app/core/filmes.service.ts
@@ -4,15 +4,21 @@ import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Filme } from '../shared/models/filme';
 
+const apiUrl = environment.apiUrl;
+
 @Injectable({
   providedIn: 'root'
 })
 export class FilmesService {
-  
+
   constructor(private http: HttpClient) { }
 
   salvar(filme: Filme): Observable<Filme> {
-    return this.http.post<Filme>(`${environment.apiUrl}/filmes`, filme);
+    return this.http.post<Filme>(`${apiUrl}/filmes`, filme);
+  }
+
+  listar(): Observable<Filme[]> {
+    return this.http.get<Filme[]>(`${apiUrl}/filmes`);
   }
 
 }
